22c80039c32516b5fde5fb88ef34e7cdebf6ac6f
Rinni Swift <rinradaswift@gmail.com>
2018-12-18 18:57:27 -0800

    Dec18 first three questions

---
+++ ChristmasInterviewPrep.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+/*
+    question 1) given an array of Int, return the largest product of 3 integers
+    question 2) calculate the indexes of the ocean view apartments
+                 E.g: Input [4, 4, 2, 3, 1] Output [1, 3, 4]
+                  ___   ___
+                  |  |  |  |        ___
+                  |  |  |  |  ___   |  |
+                  |  |  |  |  |  |  |  |  ___
+                  |  |  |  |  |  |  |  |  |  |
+                                             ~~~~~~
+    question 3) create a binary search function that takes in an array and returns a bool
+ */
